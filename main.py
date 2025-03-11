e9179494f3a7024fbca3828b5ee87a4ead080a3b
Karthik06-Git <karthik.gitacc@gmail.com>
2025-03-11 23:26:33 +0530

    data ingestion step added

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.ML_Project import logger
+from src.ML_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to custom logging")
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed! <<<< \n\n x===========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/ML_Project/utils/common.py
+++ src/ML_Project/utils/common.py
@@ -41,7 +41,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox :
 
 
 @ensure_annotations
-def create_directories(path_to_directories: list, verbose: True):
+def create_directories(path_to_directories: list, verbose=True):
     """
     creates a list of directories
 
