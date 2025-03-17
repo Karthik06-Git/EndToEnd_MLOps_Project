95b880dc176ada7618730161fd95154685509d0d
Karthik06-Git <karthik.gitacc@gmail.com>
2025-03-17 16:15:16 +0530

    data validation added

--- src/ML_Project/config/configuration.py
+++ src/ML_Project/config/configuration.py
@@ -1,6 +1,7 @@
 from src.ML_Project.constants import *
 from src.ML_Project.utils.common import read_yaml, create_directories
-from src.ML_Project.entity.config_entity import DataIngestionConfig
+from src.ML_Project.entity.config_entity import (DataIngestionConfig,
+                                                 DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -25,3 +26,16 @@ class ConfigurationManager:
                 unzip_dir= config.unzip_dir 
           )
           return data_ingestion_config   
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+          config = self.config.data_validation
+          schema = self.schema.COLUMNS
+          create_directories([config.root_dir])         
+
+          data_validation_config = DataValidationConfig(
+                root_dir= config.root_dir,
+                STATUS_FILE= config.STATUS_FILE,
+                unzip_data_dir= config.unzip_data_dir,
+                all_schema = schema
+          )
+          return data_validation_config 
