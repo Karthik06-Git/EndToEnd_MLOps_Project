440618ca93b60564d45d90b8aa2304f2d8f4335b
Karthik06-Git <karthik.gitacc@gmail.com>
2025-03-23 23:32:53 +0530

    data transformation updated

--- src/ML_Project/components/data_transformation.py
+++ src/ML_Project/components/data_transformation.py
@@ -2,10 +2,11 @@ import os
 import pandas as pd
 import numpy as np
 from src.ML_Project import logger
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
 import pickle
 from src.ML_Project.entity.config_entity import DataTransformationConfig
+
 
 
 
@@ -14,18 +15,29 @@ class DataTransformation:
     def __init__(self, config: DataTransformationConfig):
         self.config = config
 
-    """Note:- we can add any other data tranformations like Scaling, Encoding, PCA, etc..."""
+    """Note:- Data tranformations like Scaling, Encoding, PCA, etc... should be performed here"""
  
     def transform(self, data: pd.DataFrame):
+        # seperate target & features
+        target = data[["selling_price"]]
+        data.drop(["selling_price"], axis=1, inplace=True)
+
+        # transform the "year" into "Car's-age" (age = current_year-year_of_car)
+        current_year = 2025
+        data["year"] = current_year - data["year"]
+        data.rename(columns={"year":"age"}, inplace=True)
+
         num_cols = data.select_dtypes(include=["int64","float64"]).columns.tolist()
         cat_cols = data.select_dtypes(include=["object"]).columns.tolist()
 
-        # removing "brand"-column due to its high cardinality of unique-categories
-        cat_cols.remove("brand")
+        # initialize Standard-Scaler for features & target seperately
+        feature_scaler = StandardScaler()
+        data[num_cols] = feature_scaler.fit_transform(data[num_cols])
 
-        # Frequency Encoding on "brand"
-        freq_encoding = data["brand"].value_counts() / len(data)
-        data["brand"] = data["brand"].map(freq_encoding)  
+        target_scaler = StandardScaler()
+        target["selling_price"] = target_scaler.fit_transform(target).reshape(-1,1)
+
+        logger.info("Numerical columns scaled successfully!")
 
         # initialize One-Hot-Encoder
         encoder = OneHotEncoder(
@@ -37,17 +49,19 @@ class DataTransformation:
         # Perform Encoding on Categorical-columns
         encoded_cat_cols = encoder.fit_transform(data[cat_cols])
         encoded_cat_cols_df = pd.DataFrame(encoded_cat_cols, columns=encoder.get_feature_names_out())
-        transformed_data = pd.concat([data[["brand"]], encoded_cat_cols_df, data[num_cols]], axis=1)
-
-        logger.info("Categorical columns of data encoded successfully!")
-
-        # transform the "year" into "Car's-age" (age = current_year-year_of_car)
-        current_year = 2025
-        transformed_data["year"] = current_year - transformed_data["year"]
-        transformed_data.rename(columns={"year":"age"}, inplace=True)
         
-        logger.info("Year column transformed to Car's-Age")
+        transformed_data = pd.concat([encoded_cat_cols_df, data[num_cols], target[["selling_price"]]], axis=1)
 
+        logger.info("Categorical columns encoded successfully!")
+
+        # save the Std-scaler as pickle file
+        with open("artifacts/data_transformation/features_std_scaler.pkl", "wb") as scaler_file_path:
+            pickle.dump(feature_scaler, scaler_file_path)
+        with open("artifacts/data_transformation/target_std_scaler.pkl", "wb") as scaler_file_path:
+            pickle.dump(target_scaler, scaler_file_path)
+
+        logger.info("Saving both Std-scalers as pickle files at artifacts...")
+ 
         # save the OHE-encoder as pickle file
         with open("artifacts/data_transformation/OHE_encoder.pkl", "wb") as encoder_file_path:
             pickle.dump(encoder, encoder_file_path)
